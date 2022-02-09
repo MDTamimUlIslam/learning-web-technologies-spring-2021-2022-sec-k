26c46da9db4a745a118a29a714442ac48c378f4f
unknown <tamimullislam2012@gmail.com>
2022-02-09 11:28:28 +0600

    ADDED

---
+++ MID_LAB_TASK_04_PHP/task2.php
@@ -0,0 +1,13 @@
+<?php
+  echo "Welcome to the shop\n","....Calculation...\n";
+     $ProductPrice=100;
+     $TAX = 15;
+
+
+     $TAXPAY = ($ProductPrice / 100) * $TAX;
+    
+     echo "Please give \n";
+
+     echo "$TAXPAY","as TAX \n","Thank You \n";
+
+?>
--- php/task1.php
+++
@@ -1,8 +0,0 @@
-<?php
-     
-     $The_area_of_a_Rectangle =30 * 20;
-     echo "$The_area_of_a_Rectangle";
-
-     $perimeter = 2 * (30 + 20);
-     echo" $perimeter";
-?>
