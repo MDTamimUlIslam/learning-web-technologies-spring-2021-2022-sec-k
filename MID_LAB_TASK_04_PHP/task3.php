941b3c98ad018fe56bfb26e8fb9cc24985657a40
unknown <tamimullislam2012@gmail.com>
2022-02-09 11:42:13 +0600

    NEW TASK ADDED

---
+++ MID_LAB_TASK_04_PHP/task3.php
@@ -0,0 +1,13 @@
+<?php 
+    $Number=8;
+    if ($Number%2==0) {
+    	echo "ITS AN EVEN NUMBER";	
+    }
+    else if ($Number%2 !==0)
+    {
+    	echo"ITS AN ODD NUMBER";
+    }
+    else
+    echo "Not a number";
+
+?>
