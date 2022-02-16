b2b054c669a1a04d665320bd7b0b2e4cfa1cd031
unknown <tamimullislam2012@gmail.com>
2022-02-16 12:04:11 +0600

    File added

---
+++ MID_LAB_TASK_06/Public Home.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Page 1</title>
+    </head>
+    <body>
+        <table border="1" width="100%">
+            
+            <tr height="100px">
+                <td colspan="3">    
+               <h1>
+                 <span style="font-size:40px;color:green"><b>X</b></span><span style="font-size:25px">Company</span> &nbsp &nbsp &nbsp </h1>
+                </td>
+                
+            </tr>
+            <tr height="200px">
+            <td><Center> Welcome to  <span style="font-size:40px;color:green"><b>X</b></span><span style="font-size:25px">Company</span> &nbsp &nbsp &nbsp </h1></Center></td>
+            </tr>
+            <tr height="50px">
+                <td colspan="3"><Center> CopyRight@tamim2022</Center> </td>
+
+            </tr>
+        
+        </table>
+    </body>
+</html>
